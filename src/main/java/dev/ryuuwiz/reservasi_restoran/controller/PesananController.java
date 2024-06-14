a66f217635247d77ab00dfbf3a0f19343d00938d
Ryu K <ryuuwiz@gmail.com>
2024-06-14 09:53:42 +0700

    feat: ubah pesanan

--- src/main/java/dev/ryuuwiz/reservasi_restoran/controller/PesananController.java
+++ src/main/java/dev/ryuuwiz/reservasi_restoran/controller/PesananController.java
@@ -37,12 +37,28 @@ public class PesananController {
     return "tambah-pesanan";
   }
 
+  @GetMapping("/reservasi/{id}/pesanan/ubah/{id_pesanan}")
+  public String ubahPesananView(@PathVariable Map<String, String> pvMap, Model model) {
+    model.addAttribute("reservasi", reservasiService.satu(Integer.valueOf(pvMap.get("id"))));
+    model.addAttribute("pesanan", pesananService.satu(Integer.valueOf(pvMap.get("id_pesanan"))));
+    return "ubah-pesanan";
+  }
+
   @PostMapping("/reservasi/{id}/pesanan/simpan")
   public String simpan(@PathVariable("id") Integer id, @Valid PesananDto pesananDto, BindingResult result) {
     if (result.hasErrors()) {
       return "tambah-pesanan";
     }
     pesananService.simpan(id, pesananDto);
+    return "redirect:/reservasi/{id}/pesanan";
+  }
+
+  @PostMapping("/reservasi/{id}/pesanan/ubah/{id_pesanan}")
+  public String ubah(@PathVariable Map<String, String> pvMap, @Valid PesananDto pesananDto, BindingResult result) {
+    if (result.hasErrors()) {
+      return "ubah-pesanan";
+    }
+    pesananService.ubah(Integer.valueOf(pvMap.get("id_pesanan")), pesananDto);
     return "redirect:/reservasi/{id}/pesanan";
   }
 
--- src/main/java/dev/ryuuwiz/reservasi_restoran/service/PesananService.java
+++ src/main/java/dev/ryuuwiz/reservasi_restoran/service/PesananService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import dev.ryuuwiz.reservasi_restoran.dto.Mapper;
 import dev.ryuuwiz.reservasi_restoran.dto.PesananDto;
+import dev.ryuuwiz.reservasi_restoran.dto.ReservasiDto;
 import dev.ryuuwiz.reservasi_restoran.model.Pesanan;
 import dev.ryuuwiz.reservasi_restoran.model.Reservasi;
 import dev.ryuuwiz.reservasi_restoran.repository.PesananRepo;
@@ -33,6 +34,20 @@ public class PesananService {
     Optional<Reservasi> reservasi = reservasiRepo.findById(id);
     List<Pesanan> pesananByReservasi = pesananRepo.findByReservasi(reservasi.get());
     return pesananByReservasi.stream().map(Mapper::mapPesananDto).collect(Collectors.toList());
+  }
+
+  public PesananDto satu(Integer id) {
+    Optional<Pesanan> satu = pesananRepo.findById(id);
+    return Mapper.mapPesananDto(satu.get());
+  }
+
+  public PesananDto ubah(Integer id_pesanan, PesananDto pesananDto) {
+    Optional<Pesanan> pesanan = pesananRepo.findById(id_pesanan);
+    Pesanan _pesanan = pesanan.get();
+    _pesanan.setNama_pesanan(pesananDto.getNama_pesanan());
+    _pesanan.setJumlah(pesananDto.getJumlah());
+    _pesanan.setHarga(pesananDto.getHarga());
+    return Mapper.mapPesananDto(pesananRepo.save(_pesanan));
   }
 
   public void hapus(Integer id) {
